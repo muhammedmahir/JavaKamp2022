53db591477bb45334a73fac66213477ceb0cb2bd
muhammedmahir <muhammedmahir@gmail.com>
2022-10-09 18:33:42 +0300

    first

---
+++ inheritance/src/inheritance/Main.java
@@ -0,0 +1,11 @@
+package inheritance;
+
+public class Main {
+	public static void main() {
+		Customer customer = new Customer();
+		Employee employee = new Employee();
+		
+		EmployeeManager employeeManager = new EmployeeManager();
+		CustomerManager customerManager = new CustomerManager();
+	}
+}
