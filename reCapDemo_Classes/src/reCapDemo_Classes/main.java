808bbe500da17e49bfa77f7c9998b9136e86e11a
muhammedmahir <muhammedmahir@gmail.com>
2022-10-09 11:00:00 +0300

    first

---
+++ reCapDemo_Classes/src/reCapDemo_Classes/main.java
@@ -0,0 +1,13 @@
+package reCapDemo_Classes;
+
+public class main {
+
+	public static void main(String[] args) {
+		DortIslem dortIslem = new DortIslem();
+		int sonuc = dortIslem.Topla(2,3);
+		System.out.println(sonuc);
+		
+
+	}
+
+}
